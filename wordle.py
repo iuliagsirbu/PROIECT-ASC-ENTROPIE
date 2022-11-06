95ec9dedde4dd42a0de509a05332ee537ab923cc
iuliagsirbu <sirbuiuliageorgiana@gmail.com>
2022-11-06 22:35:45 +0200

    culori in terminal => idee de optimizare

--- wordle.py
+++ wordle.py
@@ -1,11 +1,18 @@
 import ast
 import program2
+BG_RED = "\u001b[41m"
+BG_BBLUE = "\u001b[44;1m"
+BG_YELLOW = "\u001b[43m"
+BG_GRAY = "\u001b[48;5;240m"
+BG_GREEN = "\u001b[42m"
+RESET = "\u001b[0m"
 cuv = "ACASA"
+#cprint(cuv, 'red')
 guess = ""
 with open("cuvordentropie.txt", "r") as k:
     guess = k.readline()
 dicty = {}
-
+print(f"{BG_BBLUE}{cuv}{RESET}")
 with open("test.txt", "r") as l:
     data = l.read()
 dicty = ast.literal_eval(data)
@@ -16,16 +23,19 @@ ok = 0
 if __name__ == "__main__":
     while ok==0:
         nr_incercari += 1
-        print("guess este ", guess, " dictionarul este ", dicty)
+        print("\n")
+        #print("guess este ", guess, " dictionarul este ", dicty)
         ok2 = 1
         for i in range(5):
             if cuv[i] == guess[i]:
+                print(f"{BG_GREEN}{cuv[i]}{RESET}", end = "")
                 for x in dicty:
                     if x!=cuv[i]:
                         list = dicty[x]
                         list[i] = 0
                         dicty[x] = list
             else:
+                print(f"{BG_RED}{guess[i]}{RESET}", end = "")
                 ok2 = 0
                 for x in dicty:
                     if x == guess[i]:
@@ -39,4 +49,4 @@ if __name__ == "__main__":
             guess = k.readline()
         if ok2 == 1:
             ok = 1
-    print(nr_incercari)
+    print("\n", nr_incercari)
