50ea27b526904f4878a500b9ee0ea01774ac75bf
iuliagsirbu <sirbuiuliageorgiana@gmail.com>
2022-11-07 15:15:12 +0200

    optimizare reguli de joc si entropie => alte idei de optimizare

--- wordle.py
+++ wordle.py
@@ -6,7 +6,7 @@ BG_YELLOW = "\u001b[43m"
 BG_GRAY = "\u001b[48;5;240m"
 BG_GREEN = "\u001b[42m"
 RESET = "\u001b[0m"
-cuv = "ACASA"
+cuv = "BANDA"
 #cprint(cuv, 'red')
 guess = ""
 with open("cuvordentropie.txt", "r") as k:
@@ -27,6 +27,30 @@ if __name__ == "__main__":
         #print("guess este ", guess, " dictionarul este ", dicty)
         ok2 = 1
         for i in range(5):
+            if guess[i] in cuv:
+                if cuv[i]==guess[i]:
+                    print(f"{BG_GREEN}{cuv[i]}{RESET}", end = "")
+                    for x in dicty:
+                        if x!=cuv[i]:
+                            list = dicty[x]
+                            list[i] = 0
+                            dicty[x] = list
+                elif cuv[i]!=guess[i]:
+                    print(f"{BG_YELLOW}{guess[i]}{RESET}", end = "")
+                    list = dicty[guess[i]]
+                    list[i] = 0
+                    dicty[guess[i]] = list
+            elif guess[i] not in cuv:
+                print(f"{BG_RED}{guess[i]}{RESET}", end = "")
+                for x in dicty:
+                    if x == guess[i]:
+                        list = dicty[x]
+                        list = [0]*5
+                        dicty[x] = list
+            with open("frecvactualizata.txt", "w") as v:
+                    print(dicty, file = v)
+            program2.run()
+        """for i in range(5):
             if cuv[i] == guess[i]:
                 print(f"{BG_GREEN}{cuv[i]}{RESET}", end = "")
                 for x in dicty:
@@ -44,7 +68,10 @@ if __name__ == "__main__":
                         dicty[x]=list
                 with open("frecvactualizata.txt", "w") as v:
                     print(dicty, file = v)
-                program2.run()
+            program2.run()"""
+        for i in range(5):
+            if cuv[i]!=guess[i]:
+                ok2=0
         with open("cuvordentropie.txt", "r") as k:
             guess = k.readline()
         if ok2 == 1:
