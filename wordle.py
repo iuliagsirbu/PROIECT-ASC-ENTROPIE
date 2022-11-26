62dd0203faf655325d7668ba12d7026a21da7d2d
iuliagsirbu <sirbuiuliageorgiana@gmail.com>
2022-11-27 01:15:23 +0200

    optimizare selectie cuvinte => progres nr de incercari

--- wordle.py
+++ wordle.py
@@ -10,45 +10,55 @@ BG_GREEN = "\u001b[42m"
 RESET = "\u001b[0m"
 
 plotvalues = {}
+alph = {}
+okalph = 0
+for i in range(65,91):
+    alph[chr(i)] = [0,5]
 
-s = open("solutii.txt", "w")
+s = open("solutii_low.txt", "w")
 
-with open("cuvinte.txt", "r") as c:
+with open("cuvinte_low.txt", "r") as c:
     for cuv in c:
         print(cuv)
         program2.reset()
 
-        guess = ""
-        with open("cuvordentropie.txt", "r") as k:
-            guess = k.readline()
         dicty = {}
         word = cuv.strip("\n")
-        s.write(word +  ", ")
+        s.write(word + ", ")
         #print(f"{BG_BBLUE}{cuv}{RESET}", end= "")
         with open("test.txt", "r") as l:
             data = l.read()
         dicty = ast.literal_eval(data)
 
+        guess = ""
+        with open("cuvordentropie.txt", "r") as k:
+            guess = k.readline()
+
         nr_incercari = 0
         ok = 0
 
         if __name__ == "__main__":
-            while ok==0:
+            while ok == 0:
+                alph = alph.fromkeys(alph, [0,5])
                 nr_incercari += 1
                 #print("\n")
                 #print("guess este ", guess, " dictionarul este ", dicty)
                 ok2 = 1
                 for i in range(5):
                     if guess[i] in cuv:
-                        if cuv[i]==guess[i]:
+                        if cuv[i] == guess[i]:
+                            alph[guess[i]] = [1,i]
+                            okalph = 1
                             s.write(cuv[i])
                             #print(f"{BG_GREEN}{cuv[i]}{RESET}", end = "")
                             for x in dicty:
-                                if x!=cuv[i]:
+                                if x != cuv[i]:
                                     list = dicty[x]
                                     list[i] = 0
                                     dicty[x] = list
-                        elif cuv[i]!=guess[i]:
+                        elif cuv[i] != guess[i]:
+                            alph[guess[i]] = [1,5]
+                            okalph = 1
                             s.write(guess[i])
                             #print(f"{BG_YELLOW}{guess[i]}{RESET}", end= "")
                             list = dicty[guess[i]]
@@ -57,24 +67,37 @@ with open("cuvinte.txt", "r") as c:
                     elif guess[i] not in cuv:
                         s.write(guess[i])
                         #print(f"{BG_RED}{guess[i]}{RESET}", end ="")
-                        for x in dicty:
-                            if x == guess[i]:
-                                list = dicty[x]
-                                list = [0]*5
-                                dicty[x] = list
+                        list = dicty[guess[i]]
+                        list = [0]*5
+                        dicty[guess[i]] = list        
                     with open("frecvactualizata.txt", "w") as v:
-                            print(dicty, file = v)
+                        print(dicty, file=v)
                     program2.run()
                 for i in range(5):
-                    if cuv[i]!=guess[i]:
-                        ok2=0
-                with open("cuvordentropie.txt", "r") as k:
-                    guess = k.readline()
+                    if cuv[i] != guess[i]:
+                        ok2 = 0
+                with open("cuvordentropie.txt", "r") as k: 
+                    okguess = 0
+                    okpoz = 1
+                    if okalph == 1:
+                        while okguess != 1:
+                            nr1=0
+                            nr2=0
+                            guess = k.readline()
+                            for let in alph:
+                                verif = alph[let]
+                                if verif[0] == 1:
+                                    nr1 += 1
+                                    if let in guess:
+                                        nr2+=1
+                            if nr1 == nr2 and okpoz==1:
+                                okguess = 1
+                    else:
+                        guess = k.readline()
                 if ok2 == 1:
                     ok = 1
                 s.write(", ")
                 #print(end=", ")
-            nr_incercari = nr_incercari - 1
             s.write(str(nr_incercari))
             if nr_incercari in plotvalues:
                 plotvalues[nr_incercari] += 1
@@ -86,17 +109,17 @@ s.close()
 nrghiciri = []
 nrcuvinte = []
 mean = 0
-#print(plotvalues)
+# print(plotvalues)
 newplotvalues = collections.OrderedDict(sorted(plotvalues.items()))
 plotvalues = newplotvalues
-#print(plotvalues)
+# print(plotvalues)
 for key in plotvalues.keys():
-    #print(key)
+    # print(key)
     nrghiciri.append(key)
     nrcuvinte.append(plotvalues[key])
 for i in range(len(nrghiciri)):
     mean = mean + nrghiciri[i] * nrcuvinte[i]
 mean = mean / sum(nrcuvinte)
-plt.bar(range(len(plotvalues)), nrcuvinte, tick_label = nrghiciri)
+plt.bar(range(len(plotvalues)), nrcuvinte, tick_label=nrghiciri)
 plt.title(f"Numarul de incercari necesare este {mean}")
 plt.show()
